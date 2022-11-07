11602dfb2f308a7e5f005862dc32af75f28933c8
LowkeyCoding <loke0046@gmail.com>
2022-11-07 21:23:23 +0100

    Added args

--- src/main.py
+++ src/main.py
@@ -1,15 +1,42 @@
+import os
+import glob
+import pathlib
 from consts import *
-from model import Params, Model
+from model import Model
 from callback import Cb1
 from loss import MusicLoss
+from args import parser, Train, Generate, Base
 
 def main():
-    params = Params()
+    args = parser.parse_args()
+    params = args.params
+    mode = args.mode.mode
+
     model = Model(params=params, pitch_loss=MusicLoss(params.batch_size),
-            step_loss=mean_squared_error, duration_loss=mean_squared_error,
-            optimizer = tf.keras.optimizers.Adam(learning_rate=params.learning_rate))
-    model.create_model()
-    model.summary()
-    model.train_model("guud_model_yes", callback = Cb1())
+        step_loss=mean_squared_error, duration_loss=mean_squared_error,
+        optimizer = tf.keras.optimizers.Adam(learning_rate=params.learning_rate))
+    if type(mode) is Train:
+        mode: Train = mode
+        model.data_dir = mode.data
+        model.create_model()
+        model.summary()
+        model.train_model(mode.name, callback = Cb1())
+    elif type(mode) is Generate:
+        mode: Generate = mode
+        model.load(mode.name)
+        if os.path.isdir(mode.input):
+            files = glob.glob(str(pathlib.Path(mode.input)/"**/*.mid*"), recursive=True)
+            if not os.path.exists(mode.output):
+                os.mkdir(mode.output)
+            for file in files:
+                for i in range(0, mode.amount):
+                    model.generate_notes(file, f"{mode.output}/{i}-{os.path.basename(file)}")
+        elif mode.amount > 1:
+            if not os.path.exists(mode.output):
+                os.mkdir(mode.output)
+            for i in range(0, mode.amount):
+                model.generate_notes(mode.input, f"{mode.output}/{i}-{os.path.basename(mode.input)}")
+        else:
+            model.generate_notes(mode.input, mode.output)
 
 main()
--- src/model.py
+++ src/model.py
@@ -8,28 +8,29 @@ import collections
 import json
 import os
 from callback import Callback
-from dataclasses import *
+from dataclasses import dataclass, asdict
 from consts import *
-
+#(16 * 120 / 60)
 @dataclass
 class Params:
-    epochs:                 int   = 50
-    sequence_length:        int   = 64
-    batch_size:             int   = 50
-    learning_rate:          float = 0.001
-    pitch_loss_scaler:      float = 0.05
-    step_loss_scaler:       float = 1.0
-    duration_loss_scaler:   float = 1.0
-    vocab_size:             int   = 128
-    file_count:             int   = 7
-    files_offset:           int   = 500
-    epochs_between_samples: int   = 10
-    samples_per_epoch:      int   = 5
-    sample_location:        int   = 505
-    notes_per_sample:       int   = 200
-    sample_temprature:      int   = 2
-    steps_per_seconds:      int   = 1#(16 * 120 / 60)
-    normalization:          int   = 0.25
+    """ Parameters for ajusting the model """
+    epochs:                 int   = 50      # The amount of Epochs.
+    sequence_length:        int   = 64      # The amount of notes per sequence.
+    batch_size:             int   = 50      # The batch size.
+    learning_rate:          float = 0.001   # The learning rate.
+    pitch_loss_scaler:      float = 0.05    # The amount to scale the pitch loss.
+    step_loss_scaler:       float = 1.0     # The amount to scale the step loss.
+    duration_loss_scaler:   float = 1.0     # The amount to scale the duration loss.
+    vocab_size:             int   = 128     # The amount of pitches in midi file DONT CHANGE!
+    file_count:             int   = 7       # The amount of files to extract notes from.
+    files_offset:           int   = 500     # The file to start from in the dataset.
+    epochs_between_samples: int   = 10      # The amount of epochs between generating sample midi files.
+    samples_per_epoch:      int   = 5       # The amount of midi file samples.
+    sample_location:        int   = 505     # The file to start from in the dataset when generating samples.
+    notes_per_sample:       int   = 200     # The amount of notes to generate per sample.
+    sample_temprature:      int   = 2       # The temprature of the samples.
+    steps_per_seconds:      int   = 1       # The amount of steps per second.
+    normalization:          int   = 0.25    # The normalization value for the gradient.
 
     def summary(self) -> str:
         return f"""
@@ -52,7 +53,7 @@ steps per seconds:          {self.steps_per_seconds}
 normalization:              {self.normalization}
         """
     def to_dict(self) -> dict:
-        return dataclasses.asdict(self)
+        return asdict(self)
 
 class Model:
     params:         Params
@@ -71,10 +72,11 @@ class Model:
         self.step_loss      = step_loss
         self.duration_loss  = duration_loss
         self.optimizer      = optimizer
-        self.files          = glob.glob(str(pathlib.Path(data_directory)/'**/*.mid*'))
+        self.files          = glob.glob(str(pathlib.Path(data_directory)/"**/*.mid*"))
         self.file_names     = self.files[self.params.files_offset:self.params.files_offset+self.params.file_count]
-        self.key_order      = ['pitch', 'step', 'duration']
-    
+        self.key_order      = ["pitch", "step", "duration"]
+    def load(self, model_name):
+        self.model = tf.keras.models.load_model(f"./{model_name}/{model_name}.h5")
     def summary(self):
         print(self.params.summary())
         self.model.summary()
@@ -86,26 +88,26 @@ class Model:
         x = tf.keras.layers.LSTM(256, return_sequences=True)(input_layer)
         d1 = tf.keras.layers.Dropout(0.3)(x)
 
-        ph1 = tf.keras.layers.LSTM(128, name='pitch_hidden1')(d1)
-        sh1 = tf.keras.layers.LSTM(128, name='step_hidden1')(d1)
-        dh1 = tf.keras.layers.LSTM(128, name='duration_hidden1')(d1)
+        ph1 = tf.keras.layers.LSTM(128, name="pitch_hidden1")(d1)
+        sh1 = tf.keras.layers.LSTM(128, name="step_hidden1")(d1)
+        dh1 = tf.keras.layers.LSTM(128, name="duration_hidden1")(d1)
 
         d2 = tf.keras.layers.Dropout(0.3)(ph1)
         d3 = tf.keras.layers.Dropout(0.3)(sh1)
         d4 = tf.keras.layers.Dropout(0.3)(dh1)
 
-        ph2 = tf.keras.layers.Dense(128,  activation='sigmoid', name='pitch_hidden2')(d2)
-        sh2 = tf.keras.layers.Dense(30,   activation='relu', name='step_hidden2')(d3)
-        dh2 = tf.keras.layers.Dense(30,   activation='relu', name='duration_hidden2')(d4)
+        ph2 = tf.keras.layers.Dense(128,  activation="sigmoid", name="pitch_hidden2")(d2)
+        sh2 = tf.keras.layers.Dense(30,   activation="relu", name="step_hidden2")(d3)
+        dh2 = tf.keras.layers.Dense(30,   activation="relu", name="duration_hidden2")(d4)
 
         d5 = tf.keras.layers.Dropout(0.3)(ph2)
         d6 = tf.keras.layers.Dropout(0.3)(sh2)
         d7 = tf.keras.layers.Dropout(0.3)(dh2)
 
         output_layers = {
-            'pitch': tf.keras.layers.Dense(128, name='pitch')(d5),
-            'step': tf.keras.layers.Dense(1,  activation='relu', name='step')(d6),
-            'duration': tf.keras.layers.Dense(1, activation='relu', name='duration')(d7),
+            "pitch": tf.keras.layers.Dense(128, name="pitch")(d5),
+            "step": tf.keras.layers.Dense(1,  activation="relu", name="step")(d6),
+            "duration": tf.keras.layers.Dense(1, activation="relu", name="duration")(d7),
         }
         self.model = tf.keras.Model(input_layer, output_layers)
 
@@ -143,17 +145,17 @@ class Model:
         # Train on data
         os.mkdir(f"./{model_name}")
         for epoch in range(self.params.epochs):
-            if epoch % self.params.epochs_between_samples == 0 and epoch > 0:
+            print("epoch:", epoch)
+            self.train(training_data, callback)
+            if epoch % (self.params.epochs_between_samples-1) == 0 and epoch > 0:
                 os.mkdir(f"./{model_name}/epoch{epoch}")
                 for i in range(0, self.params.samples_per_epoch):
                     print(f"generating sample: {i+1}")
                     self.generate_notes(self.files[self.params.sample_location+i], f"./{model_name}/epoch{epoch}/{model_name}_{epoch}_{i}.mid")
-                    
-            print("epoch:", epoch)
-            self.train(training_data, callback)
+        
         self.model.save( f"./{model_name}/{model_name}.h5")
-        with open( f"./{model_name}/{model_name}.json", 'w') as f:
-            f.write(json.dumps(self.params))
+        with open( f"./{model_name}/{model_name}.json", "w") as f:
+            f.write(json.dumps(self.params.to_dict()))
     
     def train(self, training_data: tf.data.Dataset, callback: Callback):
         all_pitch_loss = 0.0
@@ -171,9 +173,9 @@ class Model:
                 logits = self.model(x_batch_train, training=True)  # Logits for this minibatch
             
                 # Compute the loss value for this minibatch.
-                pitch_loss = self.pitch_loss(y_batch_train['pitch'], logits['pitch'],  KEYS[keys[-1]]) * self.params.pitch_loss_scaler
-                step_loss = self.step_loss(y_batch_train['step'], logits['step']) * self.params.step_loss_scaler
-                duration_loss = self.duration_loss(y_batch_train['duration'], logits['duration']) * self.params.duration_loss_scaler
+                pitch_loss = self.pitch_loss(y_batch_train["pitch"], logits["pitch"],  KEYS[keys[-1]]) * self.params.pitch_loss_scaler
+                step_loss = self.step_loss(y_batch_train["step"], logits["step"]) * self.params.step_loss_scaler
+                duration_loss = self.duration_loss(y_batch_train["duration"], logits["duration"]) * self.params.duration_loss_scaler
 
                 # Use the gradient tape to automatically retrieve
                 # the gradients of the trainable variables with respect to the loss.
@@ -220,11 +222,11 @@ class Model:
 
         raw_notes = self.midi_to_notes(in_file)
         generated_notes = {
-            'pitch': [],
-            'step':  [],
-            'duration': [],
-            'start': [],
-            'end':  []
+            "pitch": [],
+            "step":  [],
+            "duration": [],
+            "start": [],
+            "end":  []
         }
 
         sample_notes = np.stack([raw_notes[key] for key in self.key_order], axis=1)
@@ -242,16 +244,16 @@ class Model:
             end = start + duration
             input_note = (pitch, step, duration)
 
-            generated_notes['pitch'].append(pitch)
-            generated_notes['step'].append(step)
-            generated_notes['duration'].append(duration)
-            generated_notes['start'].append(start)
-            generated_notes['end'].append(end)
+            generated_notes["pitch"].append(pitch)
+            generated_notes["step"].append(step)
+            generated_notes["duration"].append(duration)
+            generated_notes["start"].append(start)
+            generated_notes["end"].append(end)
 
             input_notes = np.delete(input_notes, 0, axis=0)
             input_notes = np.append(input_notes, np.expand_dims(tf.divide(input_note, [128, 1, 1]), 0), axis=0)
             prev_start = start
-        pm = self.notes_to_midi(pd.DataFrame(generated_notes, columns=(*(self.key_order), 'start', 'end')), instrument_name)
+        pm = self.notes_to_midi(pd.DataFrame(generated_notes, columns=(*(self.key_order), "start", "end")), instrument_name)
         pm.write(out_file)
 
     def predict_next_note(
@@ -267,9 +269,9 @@ class Model:
         inputs = tf.expand_dims(notes, 0)
 
         predictions = self.model.predict(inputs, verbose=0)
-        pitch_logits = predictions['pitch']
-        step = predictions['step']
-        duration = predictions['duration']
+        pitch_logits = predictions["pitch"]
+        step = predictions["step"]
+        duration = predictions["duration"]
 
         pitch_logits /= temperature
         pitch = tf.random.categorical(pitch_logits, num_samples=1)
@@ -294,9 +296,9 @@ class Model:
         for note in sorted_notes:
             start = note.start
             end = note.end
-            notes['pitch'].append(note.pitch)
-            notes['step'].append((start - prev_start) * self.params.steps_per_seconds)
-            notes['duration'].append((end - start) * self.params.steps_per_seconds)
+            notes["pitch"].append(note.pitch)
+            notes["step"].append((start - prev_start) * self.params.steps_per_seconds)
+            notes["duration"].append((end - start) * self.params.steps_per_seconds)
             prev_start = start
 
         return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
@@ -313,11 +315,11 @@ class Model:
 
         prev_start = 0
         for i, note in notes.iterrows():
-            start = float(prev_start + (note['step']  / self.params.steps_per_seconds))
-            end = float(start + (note['duration'] / self.params.steps_per_seconds))
+            start = float(prev_start + (note["step"]  / self.params.steps_per_seconds))
+            end = float(start + (note["duration"] / self.params.steps_per_seconds))
             note = pretty_midi.Note(
                 velocity=velocity,
-                pitch=int(note['pitch']),
+                pitch=int(note["pitch"]),
                 start=start,
                 end=end,
             )
@@ -333,5 +335,5 @@ class Model:
             file.close()
 
         for x in jsonObject:
-            if (x['file_path'][5:-1] == file_path[8:-1]):
-                return x['key']
+            if (x["file_path"][5:-1] == file_path[8:-1]):
+                return x["key"]
